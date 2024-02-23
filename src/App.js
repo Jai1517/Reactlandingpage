1bcc8209222bcf97b69f87068e2a49287a1c0f69
jai1517 <jai15052000@gmail.com>
2024-02-23 09:41:52 +0530

    reactproject

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='p12'>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Features</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Pricing</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Testimonials</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Resource</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>circle</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Company</ins></p>
+        <p class="col-xl-3 col-md-4 col-sm-6 centerValue"><ins>Contact</ins></p>
+       <div className='l'><button>Login</button></div>
+     </div>
+     <center>
+     <h1>A powerful online engagement tool <br></br>
+        that's intuitive and simple to use.
+     </h1>
+     <h6>with stellar one-click and unmatched support,see how <br></br>
+     circle will make a difference in your business.<br></br>
+     <button class="bg-danger g1">Get Started</button></h6>
+     
+     <img src='https://adigaskell.org/wp-content/uploads/2015/09/group-decision.jpg'/>
+     </center>
+      </div>
+      
+    
+  );
+}
+
+export default App;
